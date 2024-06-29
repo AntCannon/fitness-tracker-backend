85b824dde34429c25119dea08a9384cc6d97a660
AntCannon <anthonycannonier@gmail.com>
2024-06-29 16:43:26 -0400

    Get /workouts/:id
    1. use find method
    2. add error handling

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -6,4 +6,14 @@ workouts.get('/', (req, res) => {
   res.status(200).send(workoutArray)
 })
 
+workouts.get('/:id', (req, res) => {
+  const id = +req.params.id
+  const workout = workoutArray.find(workout => workout.id === id)
+  if (workout) {
+    res.status(200).json(workout)
+  } else {
+      res.status(404).json({error: `workout id ${id} not found`})
+  }
+})
+
 module.exports = workouts
